6b67b95fa62dcc6d9d288051e1e5d1afeda5e73e
Hilton Santos <jhilton930@gmail.com>
2020-04-15 19:24:05 -0300

    Challenge #1 Finished

--- Projects/magic8ball/lib/home.dart
+++ Projects/magic8ball/lib/home.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomePage extends StatefulWidget {
+import 'package:flutter/rendering.dart';
+
+class BallPage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _BallPageState createState() => _BallPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _BallPageState extends State<BallPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.orange[400],
+        title: Text(
+          'Ask Me Anything',
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: SafeArea(
+        child: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int _BallImage = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlatButton(
+        child: Center(
+          child: Image.asset('images/ball$_BallImage.png'),
+        ),
+        onPressed: () {
+          setState(() {
+            _BallImage = Random().nextInt(5) + 1;
+          });
+        },
+      ),
+    );
   }
 }
