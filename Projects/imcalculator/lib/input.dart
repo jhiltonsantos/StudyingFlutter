f1ac9dda32c47b7e564d78670518f1ff747f180c
Hilton Santos <jhilton930@gmail.com>
2020-05-05 09:22:49 -0300

    Update IMC Calculator

--- Projects/imcalculator/lib/input.dart
+++ Projects/imcalculator/lib/input.dart
@@ -4,12 +4,38 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'GenderForm.dart';
 import 'BoxContains.dart';
 
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inactiveCardColor;
+  Color femaleCardColor = inactiveCardColor;
+
+  void updateGenderBottom(int gender) {
+    if (gender == 0) {
+      if (maleCardColor == inactiveCardColor) {
+        maleCardColor = activeCardColor;
+        femaleCardColor = inactiveCardColor;
+      } else {
+        maleCardColor = inactiveCardColor;
+      }
+    }
+    if (gender == 1) {
+      if (femaleCardColor == inactiveCardColor) {
+        femaleCardColor = activeCardColor;
+        maleCardColor = inactiveCardColor;
+      } else {
+        femaleCardColor = inactiveCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,21 +48,35 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: BoxContains(
-                    cor: Color(0xFF1D1E33),
-                    cardChild: GenderForm(
-                        icon: FontAwesomeIcons.mars,
-                        cor: Color(0xFF8D8E98),
-                        gender: 'MASCULINO'),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateGenderBottom(0);
+                      });
+                    },
+                    child: BoxContains(
+                      cor: maleCardColor,
+                      cardChild: GenderForm(
+                          icon: FontAwesomeIcons.mars,
+                          cor: Color(0xFF8D8E98),
+                          gender: 'MASCULINO'),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: BoxContains(
-                    cor: Color(0xFF1D1E33),
-                    cardChild: GenderForm(
-                        icon: FontAwesomeIcons.venus,
-                        cor: Color(0xFF8D8E98),
-                        gender: 'FEMININO'),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateGenderBottom(1);
+                      });
+                    },
+                    child: BoxContains(
+                      cor: femaleCardColor,
+                      cardChild: GenderForm(
+                          icon: FontAwesomeIcons.venus,
+                          cor: Color(0xFF8D8E98),
+                          gender: 'FEMININO'),
+                    ),
                   ),
                 ),
               ],
@@ -62,6 +102,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-
-
