459bcd537b95049a3531130d83c4df97b824bac8
Hilton Santos <jhilton930@gmail.com>
2020-05-20 21:37:48 -0300

    Add clima

---
+++ Projects/clima_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+    );
+  }
+}
+
+
